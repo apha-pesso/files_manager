069f59f620cef179d4360fc94ddee2d6338751f9
apha-pesso <mutiuayo99@gmail.com>
2023-05-26 23:29:15 +0100

    Server and utils

---
+++ utils/db.js
@@ -0,0 +1,53 @@
+// DBClient class
+import { MongoClient } from 'mongodb';
+
+// Create the DBclient class
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+    const url = `mongodb://${host}:${port}/${database}}`;
+    this.client = new MongoClient(url, { useUnifiedTopology: true });
+	  this.status = false;
+    this.connect();
+  }
+
+  // Create async connnection function, to be called in the constructor
+  async connect() {
+    try {
+      await this.client.connect();
+      this.status = true;
+      // console.log('Connected successfuly');
+    } catch (error) {
+      console.log('Error', error);
+    }
+  }
+
+  // returns connection status
+  isAlive() {
+	  return this.status;
+  }
+
+  // returns number of users
+  async nbUsers() {
+    const nbUsers = await this.client
+      .db('files_manager')
+      .collection('users')
+      .countDocuments();
+    return nbUsers;
+  }
+
+  // return number of files in the collection
+  async nbFiles() {
+    const nbFiles = await this.client
+      .db('files_manager')
+      .collection('files')
+      .countDocuments();
+    return nbFiles;
+  }
+}
+
+// create and export BDclient instance
+const dbClient = new DBClient();
+module.exports = dbClient;
