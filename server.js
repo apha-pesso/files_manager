d98641c87cd3a4aae937851c4a9f6772577f0833
Adewole Conde <phatboislym@protonmail.com>
2023-05-29 00:12:41 +0100

    replace body parser with express.json

--- server.js
+++ server.js
@@ -1,15 +1,10 @@
-// Express server
-
 import express from 'express';
 import routes from './routes';
-
-// Get body-parser to access the request body
-const bodyParser = require('body-parser');
 
 const port = process.env.PORT || 5000;
 
 const app = express();
-app.use(bodyParser.json());
+app.use(express.json());
 
 app.use('/', routes);
 
